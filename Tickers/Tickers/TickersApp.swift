407e1714718065aeded8728936a2f7bc85ef04a9
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2023-05-23 19:51:33 -0300

    setup project

---
+++ Tickers/Tickers/TickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickersApp.swift
+//  Tickers
+//
+//  Created by Lucas Oliveira on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
