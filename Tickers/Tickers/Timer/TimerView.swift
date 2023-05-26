c37551e5f38d283e77946a52762ce8ed4a25c28f
Noyalle Pereira <alley.pereira@hp.com>
2023-05-26 18:33:24 -0300

    wip: creating components on view

--- Tickers/Tickers/Timer/TimerView.swift
+++ Tickers/Tickers/Timer/TimerView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct TimerView: View {
     
-    @State var isTimerRunning = false
+    // MARK: - Properties
+    @State var isTimerRunning: Bool = false
     @State private var progressTimer: Double = 1.0
-    @State private var startTime = Date()
-    @State private var timerString = "0:00"
+    @State var durationInSecond: TimeInterval
     
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     let color = Color(
         red: 59 / 255,
@@ -28,6 +28,31 @@ struct TimerView: View {
         blue: 249 / 255
     )
     
+    let stepTimer: TimeInterval
+    
+    init(durationInSecond: TimeInterval) {
+        self.durationInSecond = durationInSecond
+        self.stepTimer = 1/(durationInSecond)
+    }
+    
+    // MARK: -  Actions
+    func stop() {
+        timer.upstream.connect().cancel()
+    }
+    func start() {
+        timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    }
+    
+    func configureTimer(_ toRunOrReload: Bool) {
+        isTimerRunning = toRunOrReload
+        if isTimerRunning {
+            guard durationInSecond > 0 else { return }
+            start()
+        } else {
+            stop()
+        }
+    }
+    // MARK: Views
     var body: some View {
         GeometryReader { geo in
             VStack(spacing: 15) {
@@ -53,21 +78,19 @@ struct TimerView: View {
                 )
                 .animation(.spring(), value: progressTimer)
                 .overlay {
-                    Text(self.timerString)
-                        .font(.largeTitle)
-                        .foregroundColor(color)
-                    
+                    componentView
                 }
                 .onReceive(timer) { _ in
                     progressTimer -= 0.1
-                    if (progressTimer <= 0.0) {
-                        timer.upstream.connect().cancel()
+                    if (progressTimer <= 0.0 || durationInSecond <= 0) {
+                        configureTimer(true)
                     }
                 }
             }
         }
         .padding(40)
-    }
+        
+    } //: Body View
     
     // MARK: - View to create a ball on progress
     private var ballKnobView: some View {
@@ -89,10 +112,42 @@ struct TimerView: View {
                 .rotationEffect(.degrees(progressTimer * 360))
         }
     }
+    
+    
+    // MARK: View to create buttons of body view
+    private var componentView: some View {
+        VStack(spacing: 20) {
+            Text("\(durationInSecond.timeString)")
+                .font(.system(size: 60, weight: .medium, design: .rounded))
+                .foregroundColor(color)
+            HStack(alignment: .bottom, spacing: 20) {
+
+                Button {
+                    print("Reiniciando o timer")
+                } label: {
+                    Image("restartWatch")
+                }
+
+                Button {
+                    print("Iniciando o timer")
+                } label: {
+                    Image("playButton")
+                        .padding(.bottom, 10)
+                }
+        
+                Button {
+                    print("Pulando o timer")
+                } label: {
+                    Image("skipRest")
+                }
+                
+            } //: HStack
+        }//: VStack
+    }
 }
 
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
-        TimerView()
+        TimerView(durationInSecond: 12)
     }
 }
