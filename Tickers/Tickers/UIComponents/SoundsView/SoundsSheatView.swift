b506d95bc60d4e951ffc7ad3e7ba96d30358aa48
Alley Pereira <noyallepereira@gmail.com>
2023-06-07 21:02:01 -0300

    wip: Creating SoundsSheatView file

---
+++ Tickers/Tickers/UIComponents/SoundsView/SoundsSheatView.swift
@@ -0,0 +1,20 @@
+//
+//  SoundsSheatView.swift
+//  Tickers
+//
+//  Created by Alley Pereira on 07/06/23.
+//
+
+import SwiftUI
+
+struct SoundsSheatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SoundsSheatView_Previews: PreviewProvider {
+    static var previews: some View {
+        SoundsSheatView()
+    }
+}
