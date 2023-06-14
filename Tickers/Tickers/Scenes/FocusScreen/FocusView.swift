47a015c989b52ee9f336a94b6fa86ed3f6b2788b
Noyalle Pereira <alley.pereira@hp.com>
2023-06-14 12:54:13 -0300

    wip: snore animation using offsets

--- Tickers/Tickers/Scenes/FocusScreen/FocusView.swift
+++ Tickers/Tickers/Scenes/FocusScreen/FocusView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct FocusView: View {
-
+    
     // MARK: - Properties
     private let color = Color(
         red: 59 / 255,
@@ -16,7 +16,8 @@ struct FocusView: View {
         blue: 235 / 255
     )
     let durationInSecond: TimeInterval
-
+    @State private var yOffset: CGFloat = 0
+    
     init(durationInSecond: TimeInterval) {
         self.durationInSecond = durationInSecond
     }
@@ -28,7 +29,13 @@ struct FocusView: View {
                 .tickerFont(size: 90, weight: .bold)
                 .foregroundColor(color)
             SnoreView()
+                .onAppear {
+                    withAnimation(.snore) {
+                        yOffset = 20
+                    }
+                }
             Image("babyCatAsleepFocus")
+                .offset(y: yOffset)
         }
     }
 }
--- Tickers/Tickers/Scenes/FocusScreen/SnoreView.swift
+++ Tickers/Tickers/Scenes/FocusScreen/SnoreView.swift
@@ -8,15 +8,34 @@
 import SwiftUI
 
 struct SnoreView: View {
+    @State private var yOffset: CGFloat = 0
+    @State private var xOffset: CGFloat = 0
+
     var body: some View {
-        HStack {
+        HStack(spacing: 0) {
             VStack {
                 Image("snore(1)")
+                    .offset(x: xOffset * 1.5, y: yOffset * 0.7)
                 Image("snore(3)")
+                    .offset(x: xOffset * 1.5, y: yOffset * 1.1)
             }
             ZStack {
                 Image("snore(2)")
+                    .offset(x: xOffset * 1.1, y: yOffset)
+            }
+        }
+        .onAppear {
+            withAnimation(.snore) {
+                yOffset = 40
+                xOffset = 10
             }
         }
     }
+}
+
+extension Animation {
+    static let snore = Animation
+        .easeInOut(duration: 0.75)
+        .delay(0.75)
+        .repeatForever(autoreverses: true)
 }
