32da93e936e63df57124fc6afd4c43623177faca
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2023-06-23 20:49:50 -0300

    Criado sliderView

--- Tickers/Tickers/Scenes/Definitions/DefinitionsView.swift
+++ Tickers/Tickers/Scenes/Definitions/DefinitionsView.swift
@@ -2,14 +2,16 @@
 //  DefinitionsView.swift
 //  Tickers
 //
-//  Created by Ian Pacini on 07/06/23.
+//  Created by Ian Pacini on 23/06/23.
 //
 
 import SwiftUI
 
 struct DefinitionsView: View {
+    
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
