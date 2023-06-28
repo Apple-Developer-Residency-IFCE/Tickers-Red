e4c7d7da4ba7e63b3faffccedcab3ed98c370822
Alley Pereira <noyallepereira@gmail.com>
2023-06-27 21:32:00 -0300

    wip: creating AchievementsCardView

--- Tickers/Tickers/Scenes/Home/HomeView.swift
+++ Tickers/Tickers/Scenes/Home/HomeView.swift
@@ -7,6 +7,43 @@
 
 import SwiftUI
 
+//struct VerTudoBntView: View {
+//    var body: some View {
+//        Button("Ver tudo") {
+//
+//        }
+//    }
+//}
+
+//struct VerTudoBntView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        VerTudoBntView()
+//    }
+//}
+
+struct AchievementsCardView: View {
+    
+    let titleAchievement: String
+    let descriptionAchievement: String
+    
+    init(titleAchievement: String, descriptionAchievement: String) {
+        self.titleAchievement = titleAchievement
+        self.descriptionAchievement = descriptionAchievement
+    }
+    
+    var body: some View {
+        Image("")
+        Text(titleAchievement).tickerFont(size: 14, weight: .bold)
+        Text(descriptionAchievement).tickerFont(size: 12, weight: .bold)
+    }
+}
+
+struct AchievementsCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        AchievementsCardView(titleAchievement: "Hora do Foco", descriptionAchievement: "Concluiu seu primeiro Pomodoro.")
+    }
+}
+
 struct HomeView: View {
     @ObservedObject var tickersViewModel: TickersViewModel
     
