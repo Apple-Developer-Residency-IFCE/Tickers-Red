116bc77677eb725cb119101b91ab9a3d88ef2e3e
Alley Pereira <noyallepereira@gmail.com>
2023-06-28 21:05:38 -0300

    wip: creating new home view with scrollable cards

--- Tickers/Tickers/Scenes/Home/HomeView.swift
+++ Tickers/Tickers/Scenes/Home/HomeView.swift
@@ -7,80 +7,53 @@
 
 import SwiftUI
 
-//struct VerTudoBntView: View {
-//    var body: some View {
-//        Button("Ver tudo") {
-//
-//        }
-//    }
-//}
-
-//struct VerTudoBntView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        VerTudoBntView()
-//    }
-//}
-
-struct AchievementsCardView: View {
-    
-    let titleAchievement: String
-    let descriptionAchievement: String
-    
-    init(titleAchievement: String, descriptionAchievement: String) {
-        self.titleAchievement = titleAchievement
-        self.descriptionAchievement = descriptionAchievement
-    }
-    
-    var body: some View {
-        Image("")
-        Text(titleAchievement).tickerFont(size: 14, weight: .bold)
-        Text(descriptionAchievement).tickerFont(size: 12, weight: .bold)
-    }
-}
-
-struct AchievementsCardView_Previews: PreviewProvider {
-    static var previews: some View {
-        AchievementsCardView(titleAchievement: "Hora do Foco", descriptionAchievement: "Concluiu seu primeiro Pomodoro.")
-    }
-}
-
 struct HomeView: View {
     @ObservedObject var tickersViewModel: TickersViewModel
-    
     let columns = [
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
     
     var body: some View {
-        NavigationView{
-            VStack(alignment: .leading){
+        NavigationView {
+            VStack(alignment: .leading, spacing: 15) {
                 Divider()
-                Text("Seus tickers").tickerFont(size: 20, weight: .bold)
-                    .foregroundColor(.blue)
-                    .padding(.leading, 20)
-                    .padding(.bottom, -5)
                 
-                ScrollView{
-                    LazyVGrid(columns: columns, spacing: 12) {
+                ShowAllButtonView(title: "Escolha um Ticker para cuidar")
+                
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 15) {
                         ForEach(tickersViewModel.tickers) { ticker in
                             CardPetView(isLocked: ticker.isLocked, actualProgress: ticker.actualProgress, totalProgress: ticker.totalProgress, tickerLevel: ticker.tickerLevel, tickerImage: ticker.tickerImage, tickerEgg: ticker.tickerEgg, tickerName: ticker.tickerName)
                         }
-                    }//Vgrid
-                    .padding(.horizontal)
-                }//scrollView
-                Divider()
-                    .padding(.bottom, 10)
-                    .padding(.top, -7)
-            }//Vstack
-            .background{
-                Color("BackgroundColor")
-                    .padding(.bottom, 10)
+                    }
+                    .padding(.trailing, 10)
+                    .background(Color.orange)
+                }
+                .padding(.bottom, 50)
+                
+                ShowAllButtonView(title: "Conquistas")
+                
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 15) {
+                        ForEach(tickersViewModel.tickers) { ticker in
+                            CardPetView(isLocked: ticker.isLocked, actualProgress: ticker.actualProgress, totalProgress: ticker.totalProgress, tickerLevel: ticker.tickerLevel, tickerImage: ticker.tickerImage, tickerEgg: ticker.tickerEgg, tickerName: ticker.tickerName)
+                        }
+                    }
+                    .padding(.trailing, 10)
+                    .background(Color.orange)
+                }
+                .padding(.bottom, 50)
             }
-        }//navView
+            
+            .padding(.leading)
+            .background(Color.pink.opacity(0.5))
+            .navigationBarTitle("Home")
+        }
     }
 }
 
+// MARK: - PreviewProvider
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(tickersViewModel: TickersViewModel())
