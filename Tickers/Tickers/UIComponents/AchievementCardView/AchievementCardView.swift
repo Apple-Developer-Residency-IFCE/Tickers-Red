5a116b0f1e4e0d790bddcca8ab425debbf9a1f0a
Julia Morales <juliamoraless1204@gmail.com>
2023-06-29 18:48:29 -0300

    Add text colors

--- Tickers/Tickers/UIComponents/AchievementCardView/AchievementCardView.swift
+++ Tickers/Tickers/UIComponents/AchievementCardView/AchievementCardView.swift
@@ -23,21 +23,21 @@ struct AchievementCardView: View {
     var body: some View {
         HStack{
             ZStack{
-                isCompleted == true ? Image("trophyUnlocked") : Image("trophyLocked")
+                isCompleted ? Image("trophyUnlocked") : Image("trophyLocked")
             }
                 .padding(.leading, leadingPadding)
             
             VStack(spacing: 0){
                 Text(title).tickerFont(size: 16, weight: .bold)
                     .frame(maxWidth: .infinity, alignment: .leading)
-                    .foregroundColor(Color("TextColor"))
+                    .foregroundColor(isCompleted ? Color("AchievCompleted") : Color("AchievIncompleted"))
                 
                 Text(description).tickerFont(size: 14, weight: .regular)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.trailing, trailingPadding)
-                    .foregroundColor(Color("TextColor"))
+                    .foregroundColor(isCompleted ? Color("AchievCompleted") : Color("AchievIncompleted"))
                 
-                if isProgressive == true && !isCompleted {
+                if isProgressive && !isCompleted {
                     ProgressBarAchievement(taskTotal: taskTotal, taskResolved: taskResolved)
                 }
             }
@@ -46,14 +46,14 @@ struct AchievementCardView: View {
         .background{
             RoundedRectangle(cornerRadius: cornerRadius)
                 .frame(width: 355, height: 88)
-                .foregroundColor(isCompleted == true ? Color("trophyUnlocked") : Color("trophyLocked"))
+                .foregroundColor(isCompleted ? Color("trophyUnlocked") : Color("trophyLocked"))
         }
     }
 }
 
 struct AchievementCardView_Previews: PreviewProvider {
     static var previews: some View {
-        AchievementCardView(isCompleted: false, isProgressive: true, taskTotal: 5, taskResolved: 1, title: "Hora do Foco", description: "Completou um total de 5 horas em foco.")
+        AchievementCardView(isCompleted: false, isProgressive: true, taskTotal: 5, taskResolved: 1, title: "Hora do Foco", description: "Completou um total de 5 horas em foco")
     }
 }
 
