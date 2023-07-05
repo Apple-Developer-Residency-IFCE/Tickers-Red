d21ab897a2711601787019dab9b23b2148f027f9
WelingtonDeOliveira <wellington220100@gmail.com>
2023-07-04 21:03:10 -0300

    aqui estou eu mais um dia

---
+++ Tickers/Tickers/Views/AchievementsView/AchievementsScreenView.swift
@@ -0,0 +1,32 @@
+//
+//  AchivementsScreenView.swift
+//  Tickers
+//
+//  Created by Julia Morales on 29/06/23.
+//
+
+import SwiftUI
+
+struct AchievementsScreenView: View {
+    @ObservedObject var achievementViewModel = AchievementViewModel()
+    
+    var body: some View {
+        NavigationView{
+            VStack(alignment: .leading) {
+                Divider()
+                ScrollView{
+                    ForEach(achievementViewModel.achievements, id: \.self) { achievement in
+                        AchievementCardView(isCompleted: achievement.isCompleted , isProgressive: achievement.isProgressive, taskTotal: achievement.taskTotal, taskResolved: achievement.taskResolved, title: achievement.title, description: achievement.description)
+                    }
+                }
+            }
+            .navBarWithBackButton(blackText: "Conquistas")
+        }.navigationBarBackButtonHidden(true)
+    }
+}
+
+struct AchievementsScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        AchievementsScreenView()
+    }
+}
